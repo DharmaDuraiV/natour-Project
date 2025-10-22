2e4e581fcb18ac2de88256bf02151de8228791c8
DharmaDuraiV <dharmadurai280@gmail.com>
2025-10-22 19:57:59 +0530

    file updated

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const userRoutes = require('./routes/userRoutes');
 const tourRoutes = require('./routes/tourRoutes');
 
 const app = express();
-
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-
-app.use(morgan('dev'));
-
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
   req.requestedTime1 = new Date().toLocaleString('en-IN');
--- server.js
+++ server.js
@@ -1,5 +1,41 @@
-const app = require('./app');
+require('dotenv').config({ path: './config.env', quiet: true });
 
+const app = require('./app');
+const mongoose = require('mongoose');
+
+// console.log(app.get('env'));
+// console.log(process.env);
+
+const DB_URL = process.env.MONGODB_GLOBAL.replace(
+  '<PASSWORD>',
+  process.env.MONGODB_PASSWORD
+);
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log('connect to mongodb server');
+  })
+  .catch((err) => {
+    console.log('Error 💥:', err.stack);
+  });
+
+const TourSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Tour = mongoose.model('tour', TourSchema);
+
+const newtour = new Tour({
+  name: 'Dharma',
+  age: 25,
+});
+
+newtour.save().then((data) => {
+  console.log('data inserted');
+});
+// console.log('Data is inserted');
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}/`);
