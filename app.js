173cceed81cbee2aaddd1768782baa95bd8525fe
DharmaDuraiV <dharmadurai280@gmail.com>
2025-10-23 23:36:54 +0530

    advance filter

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const userRoutes = require('./routes/userRoutes');
 const tourRoutes = require('./routes/tourRoutes');
+const qs = require('qs');
 
 const app = express();
 console.log(process.env.NODE_ENV);
+
+//it is used to parse the data in post  duration[gt]=2
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.set('query parser', (str) => qs.parse(str));
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
   req.requestedTime1 = new Date().toLocaleString('en-IN');
--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -2,7 +2,40 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // console.log(req.query);
+    const queryObj = { ...req.query };
+
+    const excludingFields = ['page', 'limit', 'sort', 'fields'];
+
+    excludingFields.forEach((el) => delete queryObj[el]);
+    // console.log(queryObj);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       result: tours.length,
